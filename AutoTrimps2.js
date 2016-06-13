36e2e5b9a6a6e32f393818ffa13b2ce094b4d028
gen BTC <genBTC@hushmail.com>
2016-06-12 23:10:14 -0400

    DelayArmor being off no longer always buys armor prestiges.

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -1161,8 +1161,9 @@ function autoLevelEquipment() {
                             (getPageSetting('DelayArmorWhenNeeded') && !enoughDamage && !enoughHealth) // if neither enough dmg or health, then tis ok to buy.
                             || 
                             (getPageSetting('DelayArmorWhenNeeded') && equipmentList[equipName].Resource == 'wood')
+                            || 
+                            !getPageSetting('DelayArmorWhenNeeded')  //or when its off.
                         )
-                        || !getPageSetting('DelayArmorWhenNeeded')  //or when its off.
                     )
                 )
             ) 
