857630f44ba14e72f7ba529de8d6dc27130c61c1
genBTC <genbtc@hushmail.com>
2016-08-01 08:41:04 -0400

    kong

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -5,7 +5,8 @@
 // @description  try to take over the world!
 // @author       zininzinin, spindrjr, belaith, ishakaru, genBTC
 // @include      *trimps.github.io*
-// @include      *kongregate.com/games/GreenSatellite/trimps
+// @include      http://www.kongregate.com/games/GreenSatellite/trimps
+// @include      www.kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
 
