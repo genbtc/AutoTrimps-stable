360f4af0818723e4c73d66d04ad25efce9173c46
gen BTC <genBTC@hushmail.com>
2016-10-31 20:56:03 -0400

    install autoperks userscript.

---
+++ install-autoperks.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         AutoPerks
+// @namespace    https://github.com/genbtc/AutoTrimps
+// @version      2.2
+// @description  Automate all the trimps!
+// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @include        *trimps.github.io*
+// @include        *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+
+var apscript = document.createElement('script');
+apscript.id = 'ZXV-Perk-Calculator';
+apscript.src = 'https://genbtc.github.io/AutoTrimps/AutoPerks.js';
+document.head.appendChild(apscript);
