032cbe9e21fe48709a4f7ca7f5d75578f381c71a
gen BTC <genBTC@gmx.com>
2016-12-19 19:07:05 -0500

    Spire scryer settings no longer apply during maps for spire farming.

--- modules/scryer.js
+++ modules/scryer.js
@@ -31,7 +31,7 @@ function useScryerStance() {
     var useoverkill = getPageSetting('ScryerUseWhenOverkill');
     if (useoverkill && game.portal.Overkill.level == 0)
         setPageSetting('ScryerUseWhenOverkill', false);
-    if (useoverkill && game.global.world == 200 && game.global.spireActive && getPageSetting('ScryerUseinSpire2')==2)
+    if (useoverkill && !game.global.mapsActive && game.global.world == 200 && game.global.spireActive && getPageSetting('ScryerUseinSpire2')==2)
         useoverkill = false;
     //Overkill button being on and being able to overkill in S will override any other setting, regardless.
     if (useoverkill && game.portal.Overkill.level > 0) {
@@ -49,7 +49,7 @@ function useScryerStance() {
 
 //Any of these being true will indicate scryer should not be used, and cause the function to dump back to regular autoStance():
     //check for spire
-    use_auto = use_auto || game.global.world == 200 && game.global.spireActive && getPageSetting('ScryerUseinSpire2')!=1;
+    use_auto = use_auto || !game.global.mapsActive && game.global.world == 200 && game.global.spireActive && getPageSetting('ScryerUseinSpire2')!=1;
     //check for voids
     use_auto = use_auto || game.global.mapsActive && getCurrentMapObject().location == "Void" && !getPageSetting('ScryerUseinVoidMaps2');
     //check for maps
