52289e10595c829ef4c38557ada971df36fbd010
Unihedron <unihedron@unihedro.com>
2017-04-06 14:18:20 +0800

    wow i suck lol

--- modules/utils.js
+++ modules/utils.js
@@ -39,6 +39,8 @@ function getCorruptScale(type) {
 // Serialize automation settings
 function serializeSettings() {
     return JSON.stringify(Object.keys(autoTrimpSettings).reduce((v, k) => {
+            if (k === 'version')
+                return v[k] = el, v; // anything else not to be transformed
             const el = autoTrimpSettings[k];
             switch (el.type) {
             case 'boolean':
@@ -50,8 +52,7 @@ function serializeSettings() {
             case 'dropdown':
                 return v[k] = el.selected, v;
             }
-            return v[k] = el, v; // anything else not to be transformed
-        }), {});
+        }, {}));
 }
 
 //Saves automation settings to browser cache
