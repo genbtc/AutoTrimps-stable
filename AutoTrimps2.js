c3fa2ca25256ccac9d1c53302eb0bc19e228c8ae
coderpatsy <animperfectpatsy@gmail.com>
2017-10-04 21:46:44 -0600

    Wording

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -55,7 +55,7 @@ function printChangelog() {
     tooltip('confirm', null, 'update', '\
 <br><b style="background-color:#002b3b">10/4 v2.1.5.6p1</b>  Combat fixes, new masteries, and spires\
 <br> Added new masteries\' attack bonuses and fixed challenge<sup>2</sup> combat bonus. \
-<br> Added settings: Toggle Patience, Min Spire for AT, Max Map Bonus for Spire. \
+<br> Added settings: Enable Patience, Min Spire for AT, Max Map Bonus for Spire. \
 <br> Fixed problem with He/Hr AutoPortal delay tooltip.  \
 <br><u>Report any bugs/problems please! You can find me on Discord: <span style="background-color:#ddd;color:#222">genr8_#8163</span></u>\
 <br><a href="https://github.com/genBTC/AutoTrimps/commits/gh-pages" target="#">Check the commit history</a> (if you care)\
