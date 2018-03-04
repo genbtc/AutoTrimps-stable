3c7e68929f489d838c40f59739fd9faeb3b82f30
gen BTC <genBTC@gmx.com>
2018-03-04 06:23:13 -0500

    client-server.js - Upload ATsettings to autotrimps.site.
    Preliminary framework. Call manually with.

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -28,7 +28,7 @@ function initializeAutoTrimps() {
     document.head.appendChild(document.createElement('script')).src = base + 'NewUI2.js';
     document.head.appendChild(document.createElement('script')).src = base + 'Graphs.js';
     //Load modules:
-    var modules = ['query', 'upgrades', 'heirlooms', 'buildings', 'jobs', 'equipment', 'gather', 'autostance', 'battlecalc', 'automaps', 'autobreedtimer', 'dynprestige', 'autofight', 'scryer', 'magma', 'portal', 'other'];
+    var modules = ['query', 'upgrades', 'heirlooms', 'buildings', 'jobs', 'equipment', 'gather', 'autostance', 'battlecalc', 'automaps', 'autobreedtimer', 'dynprestige', 'autofight', 'scryer', 'magma', 'portal', 'other', 'client-server'];
     for (var i=0,len=modules.length; i<len; i++) {
         document.head.appendChild(document.createElement('script')).src = base + module + modules[i] + '.js';
     }
---
+++ modules/client-server.js
@@ -0,0 +1,73 @@
+var ATServer = 
+{
+	SERVER_IP: '207.246.77.188',
+    SERVER_HOSTNAME: 'autotrimps.site'
+}
+
+
+/*
+	Usage-example (NOTE: ASYNCHRONOUS): 
+
+	ATServer.GetID(function(id) 
+	{ 
+		console.log(id); 
+	});
+
+*/
+
+ATServer.GetID = function(callback)
+{
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function() 
+	{
+		if (this.readyState == 4 && this.status == 200) 
+		{
+			callback(JSON.parse(req.responseText).data.id);
+		}
+	}
+
+	req.open('GET', 'https://' + ATServer.SERVER_HOSTNAME, true);
+	req.setRequestHeader('req', 'get_id');
+	req.send();
+}
+
+/*
+	Usage-example (NOTE: ASYNCHRONOUS): 
+
+	ATServer.SaveData('ff6e3508-cb6a-4042-8170-8ea1178d3175', {"test": 123}, function(response) 
+	{ 
+		console.log(response); 
+	});
+
+*/
+
+ATServer.SaveData = function(id, data, callback)
+{
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function() 
+	{
+		if (this.readyState == 4 && this.status == 200) 
+		{
+			callback(JSON.parse(req.responseText));
+		}
+	}
+
+	req.open('POST', 'https://' + ATServer.SERVER_HOSTNAME + '?id=' + id, true);
+	req.setRequestHeader('req', 'save_data');
+	req.setRequestHeader("Content-Type", "application/json");
+	req.send(JSON.stringify(data));
+}
+
+ATServer.UploadSettings = function(data)
+{
+    ATServer.GetID(function(id) 
+    { 
+        ATServer.SaveData(id, data, function(response) 
+        { 
+            console.log("AutoTrimps Settings File was Uploaded for analytics/usage!");
+            console.log(response);
+        });
+    });
+}({[autoTrimpSettings.ATversion,autoTrimpSettings]})
