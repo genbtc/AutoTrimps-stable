c3f0c69c60027dda808990b8de4da904330965f5
Jonah <jonah.ahvonen@gmail.com>
2018-03-08 00:40:38 +0200

    Change more links

--- .user.js
+++ .user.js
@@ -11,5 +11,5 @@
 
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
-script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://ahvonenj.github.io/AutoTrimps/AutoTrimps2.js';
 document.head.appendChild(script);
